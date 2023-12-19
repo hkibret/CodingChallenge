0d3ce02e37bd48effd3a51ea6617af74f7c8f10f
hkibret <111743128+hkibret@users.noreply.github.com>
2023-12-19 18:14:22 -0500

    Update missingNum.py

--- missingNum.py
+++ missingNum.py
@@ -1,1 +1,17 @@
+def missingNumbers(nums):
+    total = sum(range(1, len(nums) + 3)) 
+    for num in nums:
+        total -= num
+    averageMissingValue = total // 2
+    foundFirstHalf = 0
+    foundSecondHalf = 0 
+    for num in nums: 
+        if num <= averageMissingValue: 
+            foundFirstHalf += num
+        else: 
+            foundSecondHalf += num
 
+    expectedFirstHalf = sum(range(1, averageMissingValue + 1))
+    expectedSecondHalf = sum(range(averageMissingValue + 1, len(nums) + 3))  
+
+    return [expectedFirstHalf - foundFirstHalf, expectedSecondHalf - foundSecondHalf]
